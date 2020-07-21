54a5f6b9fcd8493c103aed73364daf01b0d28ff4
ruasyuri <68450487+ruasyuri@users.noreply.github.com>
2020-07-21 11:25:14 -0300

    Create tipagem-fraca.js

---
+++ exercicios-js/fundamentos/tipagem-fraca.js
@@ -0,0 +1,22 @@
+/*
+ *	A linguagem javascript é considerada de tipagem fraca
+ *	porque os dados podem mudar de tipo facilmente, sem a
+ *	necessidade de declarar qual o tipo de cada dado.
+ */
+
+//	Declarando e exibindo a variável 'qualquer'
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+//	Exibindo o tipo da variavel 'qualquer'
+
+//	Atribuindo um novo valor e exibindo a variável 'qualquer'
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+//	Exibindo o tipo da variavel 'qualquer'
+
+//	Evitar nome genérico ou siglas
+let valor = ''
+let numero = 1
+let pqp = false	//	Produto Químico Perigoso
