bfc901abef99922527a2622e1d19d1ee032f97c5
ruasyuri <68450487+ruasyuri@users.noreply.github.com>
2020-07-21 09:51:13 -0300

    Create organizacao.js

---
+++ exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,19 @@
+/*
+ *	Javascript é organizado por sentença de códigos
+ *	que podem terminar com ';' ou não, e também por
+ *	blocos de código.
+ */
+
+//	Sentença de código
+console.log("Sentença de código")
+
+//	Bloco de código
+{
+	//	Bloco de código
+	{
+		//	Sentença de código
+		console.log("Olá");
+		//	Sentença de código
+		console.log('Mundo!')	//	Padrão do curso
+	}
+}
