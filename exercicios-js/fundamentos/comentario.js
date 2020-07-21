1bf4713303343ffacf41d7fc2ce8466af1ce615a
ruasyuri <68450487+ruasyuri@users.noreply.github.com>
2020-07-21 12:38:28 -0300

    Update comentario.js

--- exercicios-js/fundamentos/comentario.js
+++ exercicios-js/fundamentos/comentario.js
@@ -1,14 +1,14 @@
-//	Comentário de uma linha
+// Comentário de uma linha
 console.log('Linha 1')
 
 /*
-	Comentário de
-	múltiplas linhas
+ Comentário de
+ múltiplas linhas
 */
 console.log('Linha 2')
 
 /*
- *	Comentário de
- *	múltiplas linhas
+ * Comentário de
+ * múltiplas linhas
  */
 console.log('Linha 3')
